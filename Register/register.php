84b14a46e915412afb854893c1579b61588140aa
George Nicolaou <orionas.elite@gmail.com>
2022-05-21 11:22:11 +0300

    set register footer to dynamic

--- Register/register.php
+++ Register/register.php
@@ -73,44 +73,6 @@
 </br>
 </br>
 </br>
-<!-- Footer-->
-        <footer class="py-5 bg-dark">
-		<div class="container px-4 px-lg-8">
-		<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-8">
-		<ul style="margin-left: 1200px;"><a href="#" class="top" ><i class="fa fa-angle-up" style="font-size:36px"></i></a></ul>
-		
-			<li class="nav-item"><a class="nav-link" href="http://localhost/MetaShop/AboutUs/AboutUs.html"><em>About Us</em></a></li> 
-			<li class="nav-item"><a class="nav-link" href="http://localhost/MetaShop/Terms/terms"><em>Terms of Use / Privacy Policy</em></a></li>
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; MetaShop Website 2022</p></div>
-        <ul style="margin-left: 540px;">  
-            <a style="  padding: 8px;
-            font-size: 18px;
-            width: 40px;
-            text-align: center;
-            text-decoration: none;
-            background: #3B5998;
-			color: white"href="https://www.facebook.com"  target="_blank" class="fa fa-facebook"></a> 
-            <a style="  padding: 8px;
-            font-size: 18px;
-            width: 40px;
-            text-align: center;
-            text-decoration: none;
-            background: #55ACEE;
-			color: white" href="https://www.twitter.com" target="_blank" class="fa fa-twitter"></a>
-           <a style="  padding: 8px;
-           font-size: 18px;
-           width: 40px;
-           text-align: center;
-           text-decoration: none;
-			background: #d61fcd;
-			color: white"  href="https://www.instagram.com" target="_blank" class="fa fa-instagram"></a>
-        </ul>  
-			<li class="nav-item"><a class="nav-link" href="http://localhost/Metashop/Shipping/shipping.html"><em>Shipping / Transaction</em></a></li>
-			<li class="nav-item"><a class="nav-link" href="http://localhost/Metashop/Warranty/warranty.html"><em>Warranty</em></a></li>
-		</div>
-		</footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"> </script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+<?php
+include('../footer.php');
+?>
