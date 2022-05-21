b48cccc1185dd23edef99613cf0cac398e099549
George Nicolaou <orionas.elite@gmail.com>
2022-05-21 10:44:05 +0300

    add code to set remeber me cookie

--- Register/loginscript.php
+++ Register/loginscript.php
@@ -1,49 +1,39 @@
 <?php  
 require('../includes/db.php');
 include('../includes/functions.php');
+
 if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
 	
-    // Assigning POST values to variables.
-    $username = $_POST['user_id'];
-    $password = $_POST['user_pass'];
+// Assigning POST values to variables.
+$username = $_POST['user_id'];
+$password = $_POST['user_pass'];
+$encrypted_password = md5($_POST['user_pass']); // the daabase should store the password encrypted using MD5 for simplicity 
+// CHECK FOR THE RECORD FROM TABLE
+$query = "SELECT * FROM `user_login` WHERE username='$username' and password='$encrypted_password'";
+ 
+$result = mysqli_query($con, $query) or die(mysqli_error($con));
+$row=mysqli_fetch_row($result); 
+$count = mysqli_num_rows($result);
 
-    var_dump($_POST);
-}    
-/*
+if ($count == 1){
 
-$sql = "SELECT * FROM 'user_login` WHERE username='$username'";
-if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
-            $sql .= " AND password = '" . md5($_POST["user_pass"]) . "'";
-	}
-        $result = mysqli_query($conn,$sql);
-	$user = mysqli_fetch_array($result);
-	if($user) {
-			
-			// Store data in session variables
-                $_SESSION["loggedin"] = true;
-                $_SESSION["user_id"] = $user["user_id"];
-                $_SESSION["username"] = $user["username"];
-                $_SESSION["level"] = $user["level"];
+session_start();
+                            
+// Store data in session variables
+$_SESSION["loggedin"] = true;
+$_SESSION["userid"] = $row[0]; // added this to read user id
+$_SESSION["username"] = $row[1];
+$_SESSION["level"] = $row[6];
 
-			if(!empty($_POST["remember"])) {
-				setcookie ("user_login",$_SESSION["username"],time()+ (10 * 365 * 24 * 60 * 60));
-			} else {
-				if(isset($_COOKIE["user_login"])) {
-					setcookie ("user_login","");
-				}
-			}
+if ($row[6]==0) {
 
-            if ( $_SESSION["level"]==0) {
-                //Header('Location:'.getBaseUrl().'/Home');
-                } else{
-                    //Header('Location:'.getBaseUrl().'/homeadmin.html');
-                }  
-                
-                } else {
-                //echo login_error_popup("Username or password seems to be incorrect!");
-                }
-                
+Header('Location:'.getBaseUrl();.'/Home');
+} else{
+Header('Location:'.getBaseUrl();.'/homeadmin.html');
+}  
 
-
-*/
+} else {
+echo login_error_popup("Username or password seems to be incorrect!");
+}
+}
 ?>
